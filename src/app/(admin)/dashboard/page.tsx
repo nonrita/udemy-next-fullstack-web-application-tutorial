ba6576bdd7ca7aeda3458306d9f350be20c34393
nonrita <2220025@ecc.ac.jp>
2024-10-05 15:36:47 +0900

    feat: add dashboard page

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+const AdminDashboardPage = () => {
+  return (
+    <div>AdminDashboardPage</div>
+  )
+}
+
+export default AdminDashboardPage
+
+/**
+ * (main)などのように丸括弧で囲むフォルダを作ることでURLには反映されずグルーピングが可能です
+ * またこのフォルダの直下にlayout.tsxを配置することでこのフォルダ内でのみのレイアウトを作ることも可能です
+ */
