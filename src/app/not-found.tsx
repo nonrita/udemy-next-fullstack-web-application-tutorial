2f10a8854daab4fd4d5d6421312413af79891dbc
nonrita <2220025@ecc.ac.jp>
2024-10-05 15:46:11 +0900

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <div>NotFoundPage</div>
+  )
+}
+
+export default NotFoundPage
+
+/**
+ * アクセスされたページがない時にnot-found.tsxを設定していれば自分で設定したNotFoundPageを作成できる
+ */
