d0c9acd8f221cde8ac72e3ee2566931e2b3e192a
nonrita <2220025@ecc.ac.jp>
2024-10-05 16:31:49 +0900

    log: キャッシュがヒットしたかどうか確認できるようにしました

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
+};
 
 export default nextConfig;
+
+/**
+ *logging: {
+ * fetches: {
+ *   fullUrl: true
+ * }
+ *}
+ *このような設定をすることで、データ取得時にキャッシュをヒットしたかどうか確認することができます
+ */
