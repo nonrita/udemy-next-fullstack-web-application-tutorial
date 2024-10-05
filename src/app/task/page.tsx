2a0dd9b96d75b6b548212571690a51a35cecff23
nonrita <2220025@ecc.ac.jp>
2024-10-05 15:14:24 +0900

    feat: add task page

---
+++ src/app/task/page.tsx
@@ -0,0 +1,12 @@
+const TaskPage = () => {
+  return (
+    <div>task</div>
+  )
+}
+
+export default TaskPage
+
+/**
+ * appフォルダ内のpage.tsxの入っているフォルダ名がエンドポイントとリンクする
+ * page.tsxはデフォルトエクスポートでなければいけない
+ */
