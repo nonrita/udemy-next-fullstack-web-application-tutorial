e674474a40e650a8c8247821a2a81eb3d5d377a5
nonrita <2220025@ecc.ac.jp>
2024-10-05 15:47:49 +0900

    chorm: 学習したことについて追記しました

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,4 +11,5 @@ export default ErrorPage
 /**
  * エラーが発生した際にerror.tsxというファイルを実装していればそのページが表示される
  * またエラーが発生したページから一番近いerror.tsxファイルが表示されるのでdashboardでエラーが発生した際は(admin)フォルダの直下にあるerror.tsxファイルが表示される
+ * error.tsxファイルはクライアントコンポーネントでなければいけない
  */
