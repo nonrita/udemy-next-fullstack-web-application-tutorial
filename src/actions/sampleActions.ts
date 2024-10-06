acef643be46e25794c4ea3440fb29ef7c6072d16
nonrita <2220025@ecc.ac.jp>
2024-10-06 14:17:57 +0900

    feat: サーバーアクションズ側でエラーが起こった時の処理と処理中にボタンを押せないようにする処理を追加

--- src/actions/sampleActions.ts
+++ src/actions/sampleActions.ts
@@ -1,10 +1,16 @@
 'use server'
 
-export const createTask = async (taskId: number, formData: FormData) => {
+interface FormState {
+  error: string
+}
+
+export const createTask = async (taskId: number, state: FormState, formData: FormData) => {
   // DBにタスクを作成
   console.log('タスクを作成しました')
   console.log(formData.get('name'))
   console.log(taskId)
+  state.error = 'エラーが発生しました'
+  return state
 }
 
 /**
@@ -12,4 +18,8 @@ export const createTask = async (taskId: number, formData: FormData) => {
  * fetchなどの不要なJSを減らせることができパフォーマンスを向上させることができます。
  * また、コードがシンプルになり保守性が向上するといったメリットもあります。
  * フォームの情報はFormData型の値で受け取ることができ、そのほかの情報に関しては引数に設定することにより受け取ることができます。
+ */
+
+/**
+ * useFormStateを使用するためにFormState型の引数を受け取りstateを返す
  */
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,13 +1,27 @@
+'use client'
+
+import { useFormState, useFormStatus } from "react-dom"
+
 import { createTask } from "@/actions/sampleActions"
 
 const ServerActionsPage = () => {
   const taskId = 1
   const createTaskWithTaskId = createTask.bind(null, taskId)
+  const initialState = { error: '' }
+  const [state, formAction] = useFormState(createTaskWithTaskId, initialState)
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus()
+    return (
+      <button type="submit" className="bg-gray-400 ml-2 px-2 disabled:bg-gray-300" disabled={pending}>送信</button>
+    )
+  }
   return (
     <div>
-      <form action={createTaskWithTaskId}>
+      <form action={formAction}>
         <input type="text" name="name" id="name" className="bg-gray-200" />
-        <button type="submit" className="bg-gray-400 ml-2 px-2">送信</button>
+        <SubmitButton />
+        <div>{state.error}</div>
       </form>
     </div>
   )
@@ -17,4 +31,9 @@ export default ServerActionsPage
 
 /**
  * サーバーアクションズでフォームの情報以外を送る方法はバインドをすることで渡すことができます。
+ */
+
+/**
+ * useFormStateを使用することでサーバーアクションでエラーが起きたのかと言うことをとってくることできる
+ * useFormStatusを使用することでサーバーアクションズが実行中なのかをとってこれる
  */
