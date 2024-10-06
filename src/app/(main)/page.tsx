50cba42dd23ced89d57b6907fa2a80ab2824f844
nonrita <2220025@ecc.ac.jp>
2024-10-06 18:37:41 +0900

    feat: tasks apiからタスクを取得してくる関数を作成

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,22 @@
 import TaskCard from "@/components/TaskCard/TaskCard"
+import { TaskDocument } from "@/models/task"
 import Link from "next/link"
 import { MdAddTask } from "react-icons/md"
 
-const MainPage = () => {
+const getAllTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks`, {
+    cache: 'no-store'
+  })
+
+  if (response.status != 200) {
+    throw new Error()
+  }
+
+  const data = await response.json();
+  return data.tasks as TaskDocument[]
+}
+
+const MainPage = async () => {
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
