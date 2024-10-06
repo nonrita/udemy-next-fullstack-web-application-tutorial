a6210c82856cba2727ce9d53f394f91fc0b69664
nonrita <2220025@ecc.ac.jp>
2024-10-06 18:22:19 +0900

    feat: add createTask action

---
+++ src/actions/task.ts
@@ -0,0 +1,28 @@
+'use server'
+
+import { Task, TaskModel } from "@/models/task"
+import { connectDB } from "@/utils/database"
+import { redirect } from "next/navigation"
+
+export interface FormState {
+  error: string
+}
+
+export const createTask = async (state: FormState, formData: FormData) => {
+  const newTask: Task = {
+    title: formData.get('title') as string,
+    description: formData.get('description') as string,
+    dueDate: formData.get('dueDate') as string,
+    isCompleted: false,
+  }
+
+  try {
+    await connectDB()
+    await TaskModel.create(newTask)
+  } catch {
+    state.error = 'タスクの作成に失敗しました'
+    return state;
+  }
+
+  redirect('/')
+}
