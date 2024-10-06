312ec8c3a7c28dfeba70fc90a06ac18349166082
nonrita <2220025@ecc.ac.jp>
2024-10-06 15:19:39 +0900

    feat: メイン画面全体のデザインをしました

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import { MdAddTask } from "react-icons/md"
 
 const MainPage = () => {
   return (
-    <div>
+    <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header>
         <h1>All Tasks</h1>
         <Link href='/new'>
