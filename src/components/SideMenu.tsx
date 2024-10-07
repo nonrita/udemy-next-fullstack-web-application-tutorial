9efddaeaff1eba8b5f50d3926cca4908b05ae4f0
nonrita <2220025@ecc.ac.jp>
2024-10-07 09:13:04 +0900

    chorm: change titile size

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -4,7 +4,7 @@ const SideMenu = () => {
   return (
     <div className="w-56 pt-8 bg-gray-800 text-white">
       <div>
-        <h1 className="px-4 text-2xl font-bold">なたぬきのタスク</h1>
+        <h1 className="px-4 text-lg font-bold">なたぬきのタスク</h1>
         <NavList />
       </div>
     </div>
